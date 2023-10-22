d7f388fdd3e0f475beb78bf3c092a0c644f6ba02
Alfred Lim <alf1650@hotmail.com>
2023-10-22 14:29:12 +0800

    housekeep

--- simple-player-stats-jpa/src/main/java/sg/ntu/edu/simpleplayerstats/repository/StatisticRepository.java
+++
@@ -1,9 +0,0 @@
-package sg.ntu.edu.simpleplayerstats.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.ntu.edu.simpleplayerstats.entity.Statistic;
-
-public interface StatisticRepository extends JpaRepository<Statistic, Long> {
-
-}
